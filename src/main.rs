bd8b557bd4c2a3756712771ed9ee588a038c4f82
Kauê Crozara <ikauecrozara@gmail.com>
2026-08-26 00:05:48 -0300

    feat: adiciona mensagem e instrucoes no main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    println!("========================================================");
+    println!(" Bem-vindo ao minicurso de Rust e WebAssembly! ");
+    println!("========================================================\n");
+    
+    println!("Este projeto não possui um único arquivo de execução.");
+    println!("Cada exercício foi criado como um programa independente.\n");
+    
+    println!("Para rodar um exercício, use o comando abaixo no terminal:\n");
+    println!("cargo run --bin <nome_do_arquivo>\n");
+    
+    println!("Exemplo: cargo run --bin ex01_variaveis\n");
+    println!("Abra o arquivo README.md para ver a lista completa de exercícios!");
+    println!("========================================================");
 }
